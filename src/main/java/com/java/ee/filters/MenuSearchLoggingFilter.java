be65c1dbf278cd4892b4dd0c1154d422407c5fc7
Leela <leela.jagu@gmail.com>
2018-07-05 23:04:38 +0530

    Filters

---
+++ src/main/java/com/java/ee/filters/MenuSearchLoggingFilter.java
@@ -0,0 +1,34 @@
+package com.java.ee.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(value= {"/searchResults.html","/anotherUrl.html"})
+public class MenuSearchLoggingFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String searchTerm = request.getParameter("searchTerm");
+		
+		System.out.println("user searched for :" + searchTerm);
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
